163d5a5f1937e74660b453fe5cc6028b664adce4
michele <micheleiacuitto92@gmail.com>
2024-06-21 17:27:13 +0200

    ghdthd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimoComponenteComponent } from './_componenti/primo-componente/primo-componente.component';
+import { NavbarComponent } from './_componenti/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrimoComponenteComponent
+    PrimoComponenteComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
